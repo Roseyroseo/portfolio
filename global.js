605db59f42731835a562739e26725616b9625e69
Rosey G <70283993+Roseyroseo@users.noreply.github.com>
2025-01-23 19:45:05 -0800

    Update index.html
    delete class="current"
    update global.js

--- global.js
+++ global.js
@@ -3,3 +3,7 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Automatically log nav links on every page
+const navLinks = $$('#nav a');
+console.log('Navigation Links:', navLinks);
