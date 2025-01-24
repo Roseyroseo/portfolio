d9882db1575bb542ed99d1cd7c90e59d274474e9
Rosey G <70283993+Roseyroseo@users.noreply.github.com>
2025-01-23 20:53:32 -0800

    Update global.js
    absolute pathing

--- global.js
+++ global.js
@@ -4,23 +4,12 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// // Automatically log nav links on every page
-// const navLinks = $$('#nav a');
-// console.log('Navigation Links:', navLinks);
-
-// // Find the link to the current page
-// const currentLink = navLinks.find(
-//     (a) => a.host === location.host && a.pathname === location.pathname);
-
-// // Add the 'current' class to the matching link
-// currentLink?.classList.add('current'); // Safely add the class if the link exists
-
 // Navigation links data
 const pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact' },
-    { url: 'resume/', title: 'Resume' },
+    { url: '/', title: 'Home' },
+    { url: '/portfolio/projects/', title: 'Projects' },
+    { url: '/portfolio/contact/', title: 'Contact' },
+    { url: '/portfolio/resume/', title: 'Resume' },
     { url: 'https://github.com/Roseyroseo', title: 'GitHub' },
   ];
 
